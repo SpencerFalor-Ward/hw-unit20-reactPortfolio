8c5281221b3ee189214e716808995813a9a19e38
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-05-21 13:59:39 -0700

    Updates

---
+++ precache-manifest.a4411c356425a332588888bba97af4a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042b77a48526a571844edde546100195",
+    "url": "/hw-unit20-reactPortfolio/index.html"
+  },
+  {
+    "revision": "8a083821a1473a3cb39b",
+    "url": "/hw-unit20-reactPortfolio/static/js/2.ee8f51f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw-unit20-reactPortfolio/static/js/2.ee8f51f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e59851b43fef5aff40a",
+    "url": "/hw-unit20-reactPortfolio/static/js/main.e9524d8a.chunk.js"
+  },
+  {
+    "revision": "db03ed4dc14f9159fe58",
+    "url": "/hw-unit20-reactPortfolio/static/js/runtime-main.17f78239.js"
+  }
+]);
