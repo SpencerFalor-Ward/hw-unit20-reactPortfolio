09a43d67253a8ee589af90a0265af17aaa873896
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-06-22 09:43:36 -0700

    fixed broken images

--- client/src/utils/projectData.js
+++ client/src/utils/projectData.js
@@ -1,4 +1,12 @@
 import weatherImg from "../assets/images/HW-6-weatherGeneratorresize.png"
+import emojiImg from "../assets/images/HW-groupProject1-resize.png"
+import quizImg from "../assets/images/HW-4-javaQuiz-resize.png"
+import passwordImg from "../assets/images/HW-3-passwordGenerator-resize.png"
+import plannerImg from "../assets/images/HW-5-dailyPlaner-resize.png"
+import workoutImg from "../assets/images/fitnessTracker-resize.png"
+import eatAhImg from "../assets/images/eatAhBurger-resize.jpg"
+import budgetImg from "../assets/images/budgetTracker-resize.png"
+import cvImg from "../assets/images/CV 2020 resize.png"
 
 
 
@@ -13,56 +21,56 @@ var arr = [{
 },{
   url:"https://spencerfalor-ward.github.io/Bootcamp-group-project-1/",
   id:"project2",
-  src:"../assets/images/HW-groupProject1-resize.png",
+  src: emojiImg,
   rel:"placeholder",
   alt:"Emoji translator",
   title:"Collections"
 },{
   url:"https://spencerfalor-ward.github.io/HW-4-JavaScript-Quiz/",
   id:"project3",
-  src:"../../src/assets/images/HW-4-javaQuiz-resize.png",
+  src: quizImg,
   rel:"placeholder",
   alt:"JavaScript Quiz",
   title:"Games"
 },{
   url:"https://spencerfalor-ward.github.io/HW-3---passwordGenerator/",
   id:"project4",
-  src:"../../src/assets/images/HW-3-passwordGenerator-resize.png",
+  src: passwordImg,
   rel:"placeholder",
   alt:"Password Generator",
   title:"Strategic development"
 },{
   url:"https://spencerfalor-ward.github.io/HW-5-SFW-Daily-Planner/",
   id:"project5",
-  src:"../../src/assets/images/HW-5-dailyPlaner-resize.png",
+  src:plannerImg,
   rel:"placeholder",
   alt:"Daily planner",
   title:"I built this"
 },{
   url:"https://fitness-tracker-sfw.herokuapp.com/",
   id:"project6",
-  src:"../../src/assets/images/fitnessTracker-resize.png",
+  src:workoutImg,
   rel:"placeholder",
   alt:"Workout tracker",
   title:"I built this too"
 },{
   url:"https://eat-ah-burger.herokuapp.com/",
   id:"project7",
-  src:"../../src/assets/images/eatAhBurger-resize.jpg",
+  src:eatAhImg,
   rel:"placeholder",
   alt:"Eat-ah-burger",
   title:"Yup, I also built this"
 },{
   url:"https://budget-tracker-sfw.herokuapp.com/",
   id:"project8",
-  src:"../../src/assets/images/budgetTracker-resize.png",
+  src:budgetImg,
   rel:"placeholder",
   alt:"Online/Offline budget tracker",
   title:"These numbers don't lie"
 },{
   url:"./assets/Spencer Falor-Ward CV 2020.pdf",
   id:"project9",
-  src:"../../src/assets/images/CV 2020 resize.png",
+  src:cvImg,
   rel:"placeholder",
   alt:"CV",
   title:"CV"
