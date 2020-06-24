ea229a4f158805ea67c4cc1f3741704297ffcdd4
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-06-24 16:19:57 -0700

    commented out line 53 in server.js to previous code to test only indext.html manifest.json href change

--- server.js
+++ server.js
@@ -50,7 +50,8 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use("/hw-unit20-reactPortfolio/", express.static("client/build"));
+  app.use(express.static("client/build"));
+  // app.use("/hw-unit20-reactPortfolio/", express.static("client/build"));
 }
 
 
