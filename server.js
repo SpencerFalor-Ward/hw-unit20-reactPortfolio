63188600799737ea7ece9f3529e36f36a6eba5ac
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-06-24 16:28:46 -0700

    reinstated line 54 in server.js and removed homepage from client package.json

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  // app.use("/hw-unit20-reactPortfolio/", express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use("/hw-unit20-reactPortfolio/", express.static("client/build"));
 }
 
 
